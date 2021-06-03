e7bb77e348bcbee3c5cecfa51a55737203e3b655
jazabala <zabalajulian2010@hotmail.com>
2021-06-03 00:36:50 -0300

    todo andando

--- munnitorbackend/src/main/java/com/munnitorbackend/Repository/VacunaEmpresaRepo.java
+++ munnitorbackend/src/main/java/com/munnitorbackend/Repository/VacunaEmpresaRepo.java
@@ -3,6 +3,8 @@ package com.munnitorbackend.Repository;
 import com.munnitorbackend.Model.Vacuna;
 import com.munnitorbackend.Model.VacunaEmpresa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VacunaEmpresaRepo extends JpaRepository<VacunaEmpresa,Long> {
 }
